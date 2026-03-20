e306aca0a23a63882fd229712345704a86f5569d
Dev Huy <thaiquanghuy10a8.12@gmail.com>
2026-03-21 00:59:06 +0700

    fix qtable

--- Table/QTable.py
+++ Table/QTable.py
@@ -7,17 +7,17 @@ class QTable(dict):
         self.nCounts = {}
         
     def __getitem__(self, key):
-        if super.__contains__(key):
-            v = super.__getitem__(key)
+        if super().__contains__(key):
+            v = super().__getitem__(key)
             return v / self.nCounts[key]
         return self.default
     
     def __setitem__(self, key, value):
-        if super.__contains__(key):
-            old_value = super.__getitem__(key)
+        if super().__contains__(key):
+            old_value = super().__getitem__(key)
         else:
             old_value = 0
             self.nCounts[key] = 0
         
-        super.__setitem__(key, old_value + value)
+        super().__setitem__(key, old_value + value)
         self.nCounts[key] += 1
